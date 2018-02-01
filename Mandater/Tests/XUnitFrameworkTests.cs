667011121f6d6ced88f4e95bf589061bed9f447f
Log234 <ajsivesind@gmail.com>
2018-02-01 14:39:07 +0100

    Fixed an issue related to multiple entry points for the program

---
+++ Mandater/Tests/XUnitFrameworkTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mandater.Tests
+{
+    // Ensuring that xUnit behaves as expected
+    public class XUnitFrameworkTests
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3)]
+        [InlineData(5)]
+        public void IsOddTests(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        private static bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
