0a88cc7e3d75887efae28c728f1a7bc6eaca70a8
ererikse <ererikse@ifi.uio.no>
2018-03-27 19:24:04 +0200

    #27 Set up dummy HTML elements for parameter choice and begun to set up store logic with state and actions, aswell as a reducer.

--- Mandater/ClientApp/components/SettingsMenu.tsx
+++ Mandater/ClientApp/components/SettingsMenu.tsx
@@ -3,7 +3,7 @@
 export class SettingsMenu extends React.Component<{}, {}> {
     public render() {
         return <div className="settings-menu">
-            <h1 className="display-4">Stortingsvalg</h1>
+            <h1 className="h2">Stortingsvalg</h1>
             <form>
                 <div className="form-group row">
                     <label className="col-sm-5 col-form-label">År</label>
@@ -19,25 +19,30 @@ export class SettingsMenu extends React.Component<{}, {}> {
                 <div className="form-group row">
                     <label className="col-sm-5 col-form-label">Beregningsmetode</label>
                     <div className="col-sm-7">
-                        <select className="form-control" name="calcmethod">
+                        <select className="form-control" name="calcMethod">
                             <option value="SL">Sainte Lagues</option>
                         </select>
                     </div>
                 </div>
                 <div className="form-group row">
-                    <label className="col-sm-5 col-form-label">Fylke</label>
+                    <label htmlFor="firstDivisor" className="col-sm-5 col-form-label">Første delingstall</label>
                     <div className="col-sm-7">
-                        <select className="form-control" name="county">
-                            <option value="akerhus">Akershus</option>
-                        </select>
+                        <input className="form-control" classID="firstDivisor" type="number" name="firstDivisor" placeholder="1.0" min="1.0" step="0.1" max="5.0" />
                     </div>
                 </div>
                 <div className="form-group row">
-                    <label htmlFor="divnum" className="col-sm-5 col-form-label">Førstedivisor</label>
+                    <label htmlFor="electionThreshold" className="col-sm-5 col-form-label">Sperregrense</label>
                     <div className="col-sm-7">
-                        <input className="form-control" classID="divnum" type="number" name="divnum" placeholder="1.4" step="0.1" />
+                        <input className="form-control" classID="electionThreshold" type="number" name="electionThreshold" placeholder="1.4" min="0.0" step="0.1" max="15.0" />
                     </div>
-                </div>               
+                </div> 
+                <div className="form-group row">
+                    {/*Utjevningsmandat = Leveling seat as of https://www.dinordbok.no/norsk-engelsk/?q=utjevningsmandat */}
+                    <label htmlFor="levelingSeat" className="col-sm-5 col-form-label">Utjevningsmandater</label>
+                    <div className="col-sm-7">
+                        <input className="form-control" classID="levelingSeat" type="number" name="levelingSeat" placeholder="1.4" min="0.0" step="0.1" max="15.0" />
+                    </div>
+                </div> 
             </form>
         </div>
 
