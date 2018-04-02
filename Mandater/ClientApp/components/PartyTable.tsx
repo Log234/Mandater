7056bd4869c58f4dacda165b88b3d0d45c195b86
Anders Jakob Sivesind <ajsivesind@hotmail.com>
2018-04-02 17:03:54 +0200

    Partial implementation of table swap

---
+++ Mandater/ClientApp/components/PartyTable.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from 'react';
+
+export class PartyTable extends React.Component<{}, {}> {
+    public render() {
+        return (<div>
+            PartyTable
+        </div>);
+    }
+}
