b29882bee148ccb016c6c6e215f811948336266a
Edvarda-E <edvarda.eriksen@outlook.com>
2018-04-03 17:16:23 +0200

    #60 Updated navigation bar with the correct name, added a new link to the API (Swagger), and styled the navigation bar. Could only find quick-fixes of how to center the logo, so will have to look more into this later.

--- Mandater/ClientApp/components/NavMenu.tsx
+++ Mandater/ClientApp/components/NavMenu.tsx
@@ -4,8 +4,10 @@ import { NavLink, Link } from 'react-router-dom';
 export class NavMenu extends React.Component<{}, {}> {
     public render() {
         return <div className='nav_container'>
-            <ul className='nav_list'>
-                <li><a href="">Om Mandater</a></li>
+            <ul>
+                <li className="laviniaLogo"><a href="">Lavinia</a></li>
+                <li><a target="_blank" href="http://mandater-testing.azurewebsites.net/swagger/">API</a></li>
+                <li><a href="">Om Lavinia</a></li>
                 <li><a href="">Hjelp</a></li>
             </ul>
         </div>;
