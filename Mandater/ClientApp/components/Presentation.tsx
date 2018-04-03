81ce42fc62b1a08d3bace8936e97a156af8c0392
Anders Jakob Sivesind <ajsivesind@hotmail.com>
2018-04-03 12:34:07 +0200

    Small changes

---
+++ Mandater/ClientApp/components/Presentation.tsx
@@ -0,0 +1,30 @@
+﻿import * as React from 'react';
+import { ResultsTable } from './ResultsTable';
+
+type State = {
+    showGraph: string
+}
+
+export class Presentation extends React.Component<{}, State> {
+    state: State = {
+        showGraph: 'PartyTable'
+    }
+
+    // returns the corresponding View based on currentMode
+    getView() {
+        switch (this.state.showGraph) {
+            case 'ResultsTable':
+                return <ResultsTable />
+            default:
+                return <ResultsTable />
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.getView()}
+            </div>
+        );
+    }
+}
