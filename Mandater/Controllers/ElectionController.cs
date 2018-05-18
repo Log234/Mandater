3875b4afa4418e0d4e52dd046ae577582d5f5fe4
Simen <simen.d@gmail.com>
2018-05-18 02:23:39 +0200

    #57 Updated comments and indentation

--- Mandater/Controllers/ElectionController.cs
+++ Mandater/Controllers/ElectionController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using Mandater.Data;
 using Mandater.Models;
 using Mandater.Utilities;
@@ -10,6 +11,9 @@ using Microsoft.Extensions.Logging;
 
 namespace Mandater.Controllers
 {
+    /// <summary>
+    /// Controller that serves data regarding political elections in various levels, for the Mandater project at the University of Oslo, department of political science
+    /// </summary>
     [EnableCors("CorsPolicy")]
     [Produces("application/json")]
     [Route("api/v1.0.0/")]
@@ -17,120 +21,116 @@ namespace Mandater.Controllers
     {
         private readonly ElectionContext _context;
         private readonly ILogger _logger;
-
+        /// <summary>
+        /// Constructor for the ElectionController, enables database access and logging
+        /// </summary>
+        /// <param name="context">ElectionContext object that allows access to the database</param>
+        /// <param name="logger">Logger that gives information about the context of a log message</param>
         public ElectionController(ElectionContext context, ILogger<ElectionController> logger)
         {
             _context = context;
             _logger = logger;
         }
 
-        /**
-         * <summary>
-         * Default path method that returns a list of shallow Country objects, showing which countries the API has data on.
-         * If deep is specified it returns all data.
-         * </summary>
-         * <param name="deep">Optional boolean parameter, the method returns a deep list if true</param>
-         * <returns>List of countries</returns>
-         **/
+        /// <summary>
+        /// Default path method that returns a list of shallow Country objects, showing which countries the API has data on.
+        /// If deep is specified it returns all data.
+        /// </summary>
+        /// <param name="deep">Optional boolean parameter, the method returns a deep list if true</param>
+        /// <returns>List of countries</returns>
         [HttpGet]
         public IEnumerable<Country> GetCountries(bool? deep)
         {
             if (deep.HasValue && deep.Value)
+            {
                 return _context.Countries
-                    .Include(c => c.ElectionTypes)
-                    .ThenInclude(c => c.Elections)
-                    .ThenInclude(c => c.Counties)
-                    .ThenInclude(c => c.Results);
+                        .Include(c => c.ElectionTypes)
+                            .ThenInclude(c => c.Elections)
+                                .ThenInclude(c => c.Counties)
+                                    .ThenInclude(c => c.Results);
+            }
             return _context.Countries;
         }
 
-        /**
-         * <summary>
-         * Default path method that returns a list of shallow ElectionType objects, showing which election types for the specified country the API has data on.
-         * </summary>
-         * <param name="deep">Optional boolean parameter, the method returns a deep list if true</param>
-         * <param name="countryCode">Two character country code, ISO 3166-1 alpha-2</param>
-         * <returns>List of election types</returns>
-         **/
+        /// <summary>
+        /// Returns either a shallow or a deep Country object, where a deep object contains the entire hierarchy of data from the country down.
+        /// </summary>
+        /// <param name="deep">Optional boolean parameter, the method returns a deep list if true</param>
+        /// <param name="countryCode">Two character country code, ISO 3166-1 alpha-2</param>
+        /// <returns>Country</returns>
         [HttpGet("{countryCode}")]
-        public Country GetElectionTypes(string countryCode, bool? deep)
+        public Country GetCountry(string countryCode, bool? deep)
         {
             if (deep.HasValue && deep.Value)
                 return _context.Countries
                     .Include(c => c.ElectionTypes)
-                    .ThenInclude(c => c.Elections)
-                    .ThenInclude(c => c.Counties)
-                    .ThenInclude(c => c.Results)
+                        .ThenInclude(c => c.Elections)
+                            .ThenInclude(c => c.Counties)
+                            .ThenInclude(c => c.Results)
                     .First(c => c.CountryCode == countryCode.ToUpper());
-
             return _context.Countries
                 .Include(c => c.ElectionTypes)
                 .First(c => c.CountryCode == countryCode.ToUpper());
         }
 
-        /**
-         * <summary>
-         * Default path method that returns a list of shallow election objects, showing which elections for the specified country and election type the API has data on.
-         * </summary>
-         * <param name="deep">Optional boolean parameter, the method returns a deep list if true</param>
-         * <param name="countryCode">Two character country code, ISO 3166-1 alpha-2</param>
-         * <param name="electionCode">Two character election type code</param>
-         * <returns>List of elections</returns>
-         **/
+        /// <summary>
+        /// Returns either a shallow or a deep ElectionType object, where a deep object contains the entire hierarchy of data from the ElectionType down.
+        /// </summary>
+        /// <param name="deep">Optional boolean parameter, the method returns a deep list if true</param>
+        /// <param name="countryCode">Two character country code, ISO 3166-1 alpha-2</param>
+        /// <returns>ElectionType of a given country</returns>
         [HttpGet("{countryCode}/{electionCode}")]
-        public ElectionType GetElections(string countryCode, string electionCode, bool? deep)
+        public ElectionType GetElectionType(string countryCode, string electionCode, bool? deep)
         {
             if (deep.HasValue && deep.Value)
                 return _context.Countries
                     .Include(c => c.ElectionTypes)
-                    .ThenInclude(c => c.Elections)
-                    .ThenInclude(c => c.Counties)
-                    .ThenInclude(c => c.Results)
+                        .ThenInclude(c => c.Elections)
+                            .ThenInclude(c => c.Counties)
+                                .ThenInclude(c => c.Results)
                     .First(c => c.CountryCode == countryCode.ToUpper())
                     .ElectionTypes
-                    .First(c => c.InternationalName == ETNameUtilities.CodeToName(electionCode));
+                        .First(c => c.InternationalName == ETNameUtilities.CodeToName(electionCode));
 
             return _context.Countries
                 .Include(c => c.ElectionTypes)
-                .ThenInclude(c => c.Elections)
+                    .ThenInclude(c => c.Elections)
                 .First(c => c.CountryCode == countryCode.ToUpper())
                 .ElectionTypes
-                .First(c => c.InternationalName == ETNameUtilities.CodeToName(electionCode));
+                    .First(c => c.InternationalName == ETNameUtilities.CodeToName(electionCode));
         }
 
-        /**
-         * <summary>
-         * Default path method that returns a list of County objects, showing which counties for the specified election the API has data on.
-         * </summary>
-         * <param name="deep">Optional boolean parameter, the method returns a deep list if true</param>
-         * <param name="countryCode">Two character country code, ISO 3166-1 alpha-2</param>
-         * <param name="electionCode">Two character election type code</param>
-         * <param name="year">Four digit election year</param>
-         * <returns>List of counties</returns>
-         **/
+        /// <summary>
+        /// Returns either a shallow or a deep Election object, where a deep object contains the entire hierarchy of data from the Election down.
+        /// </summary>
+        /// <param name="deep">Optional boolean parameter, the method returns a deep list if true</param>
+        /// <param name="countryCode">Two character country code, ISO 3166-1 alpha-2</param>
+        /// <param name="electionCode">Two character election type code</param>
+        /// <param name="year">Four digit election year</param>
+        /// <returns>Election of a given type and a given year for a given country</returns>
         [HttpGet("{countryCode}/{electionCode}/{year}")]
-        public Election GetCounties(string countryCode, string electionCode, int year, bool? deep)
+        public Election GetElection(string countryCode, string electionCode, int year, bool? deep)
         {
             if (deep.HasValue && deep.Value)
                 return _context.Countries
                     .Include(c => c.ElectionTypes)
-                    .ThenInclude(c => c.Elections)
-                    .ThenInclude(c => c.Counties)
-                    .ThenInclude(c => c.Results)
+                        .ThenInclude(c => c.Elections)
+                            .ThenInclude(c => c.Counties)
+                                .ThenInclude(c => c.Results)
                     .First(c => c.CountryCode == countryCode.ToUpper())
-                    .ElectionTypes
-                    .First(c => c.InternationalName == ETNameUtilities.CodeToName(electionCode))
-                    .Elections
-                    .First(c => c.Year == year);
+                        .ElectionTypes
+                        .First(c => c.InternationalName == ETNameUtilities.CodeToName(electionCode))
+                            .Elections
+                            .First(c => c.Year == year);
             return _context.Countries
                 .Include(c => c.ElectionTypes)
-                .ThenInclude(c => c.Elections)
-                .ThenInclude(c => c.Counties)
+                    .ThenInclude(c => c.Elections)
+                        .ThenInclude(c => c.Counties)
                 .First(c => c.CountryCode == countryCode.ToUpper())
-                .ElectionTypes
-                .First(c => c.InternationalName == ETNameUtilities.CodeToName(electionCode))
-                .Elections
-                .First(c => c.Year == year);
+                    .ElectionTypes
+                    .First(c => c.InternationalName == ETNameUtilities.CodeToName(electionCode))
+                            .Elections
+                            .First(c => c.Year == year);
         }
     }
 }
