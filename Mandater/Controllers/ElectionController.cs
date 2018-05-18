eb93eba5a0a8306a0738b7476aafc3d2f167acd9
Simen <simen.d@gmail.com>
2018-05-18 03:01:21 +0200

    #71 Removed unnecessary usings following last commit

--- Mandater/Controllers/ElectionController.cs
+++ Mandater/Controllers/ElectionController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using Mandater.Data;
-using Mandater.Models;
 using Mandater.Utilities;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
