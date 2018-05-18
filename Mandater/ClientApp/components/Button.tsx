d773ccfa6fa4313f4baec04d97df565b8ef2c693
Simen <simen.d@gmail.com>
2018-05-18 21:46:26 +0200

    closes #83

---
+++ Mandater/ClientApp/components/Button.tsx
@@ -0,0 +1,16 @@
+﻿import * as React from "react";
+
+type ButtonProps = {
+    title: string,
+    onPress: () => any,
+    accessibilityLabel?: string,
+}
+
+const Button = (props: ButtonProps) => {
+    return <button
+               onClick={props.onPress}>
+               {props.title}
+           </button>;
+};
+
+export default Button;
