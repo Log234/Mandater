f3bde83e50a3d32045c20593ec237181d6e1547f
Simen <simen.d@gmail.com>
2018-05-21 04:12:34 +0200

    #95 Created LocalStorage with loadState and saveState functions

---
+++ Mandater/ClientApp/LocalStorage.tsx
@@ -0,0 +1,22 @@
+﻿import { ApplicationState } from "./store";
+
+export const loadState = () => {
+    try {
+        const serializedState = localStorage.getItem("state");
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (err) {
+        return undefined;
+    }
+};
+
+export const saveState = (state: ApplicationState) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem("state", serializedState);
+    } catch (err) {
+        console.error(err);
+    }
+}
