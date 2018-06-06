5ac5ffb406d81ba2ad4677f06df50fdcc18c5b7a
Unknown <ajsivesind@gmail.com>
2018-06-06 13:36:30 +0200

    Auto stash before merge of "PresentationRevamp" and "origin/PresentationRevamp"

---
+++ Mandater/ClientApp/logic/PresentationUtilities.ts
@@ -0,0 +1,44 @@
+import { PartyResult } from "../interfaces/PartyResult";
+import { roundNumber } from "./NumberUtilities";
+import { DistrictResult } from "../interfaces/DistrictResult";
+import { Column } from "react-table";
+
+export function getPartyTableData(partyResults: PartyResult[], showPartiesWithoutSeats: boolean, numberOfDecimals: number): PartyResult[] {
+    let filteredResults = [...partyResults];
+
+    if (!showPartiesWithoutSeats) {
+        filteredResults = filteredResults.filter(party => party.totalSeats > 0);
+    }
+
+    const roundedResults: PartyResult[] = [];
+
+    for (const partyResult of filteredResults) {
+        roundedResults.push({
+            partyCode: partyResult.partyCode,
+            partyName: partyResult.partyName,
+            votes: partyResult.votes,
+            percentVotes: roundNumber(partyResult.percentVotes, numberOfDecimals),
+            districtSeats: partyResult.districtSeats,
+            levelingSeats: partyResult.levelingSeats,
+            totalSeats: partyResult.totalSeats
+        });
+    }
+
+    return roundedResults;
+}
+
+export function getDistrictTableCollumns(districtResult: DistrictResult) {
+    const columns: Column[] = [];
+
+    columns.push({
+        Header: "Fylke",
+        accessor: "name"
+    });
+
+    districtResult.partyResults.forEach(partyResult => {
+        //columns.push({
+        //    Header: partyResult.partyCode,
+
+        //})
+    });
+}
