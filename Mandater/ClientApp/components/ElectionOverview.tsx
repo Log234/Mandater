3267c0c0cd37d55d9c14e2f3022ebca99a86644f
Unknown <ajsivesind@gmail.com>
2018-06-08 17:15:41 +0200

    refactor: Split tables and graphs out into separate files

--- Mandater/ClientApp/components/ElectionOverview.tsx
+++ Mandater/ClientApp/components/ElectionOverview.tsx
@@ -1,1 +1,69 @@
-﻿
+﻿import * as React from "react";
+import ReactTable from "react-table";
+import { getPartyTableData } from "../logic/PresentationUtilities";
+import { LagueDhontResult } from "../interfaces/LagueDhontResult";
+
+export interface ElectionOverviewProps {
+    decimals: number;
+    showPartiesWithoutSeats: boolean;
+    results: LagueDhontResult;
+}
+
+export class ElectionOverview extends React.Component<ElectionOverviewProps, {}> {
+    render() {
+        const results = this.props.results;
+        const showPartiesWithoutSeats = this.props.showPartiesWithoutSeats;
+        const decimals = this.props.decimals;
+
+        return (
+            <ReactTable
+                className="-highlight -striped"
+                data={getPartyTableData(
+                    results.partyResults,
+                    showPartiesWithoutSeats,
+                    decimals
+                )}
+                defaultPageSize={10}
+                showPaginationBottom={false}
+                showPaginationTop={true}
+                rowsText="rader"
+                pageText="Side"
+                ofText="av"
+                nextText="Neste"
+                previousText="Forrige"
+                columns={[
+                    {
+                        Header: "Parti",
+                        accessor: "partyName"
+                    },
+                    {
+                        Header: "Stemmer",
+                        accessor: "votes"
+                    },
+                    {
+                        Header: "Prosent",
+                        accessor: "percentVotes"
+                    },
+                    {
+                        Header: "Distrikt",
+                        accessor: "districtSeats"
+                    },
+                    {
+                        Header: "Utjevning",
+                        accessor: "levelingSeats"
+                    },
+                    {
+                        Header: "Sum",
+                        accessor: "totalSeats"
+                    }
+                ]}
+                defaultSorted={[
+                    {
+                        id: "totalSeats",
+                        desc: true
+                    }
+                ]}
+            />
+        );
+    }
+}
---
+++ Mandater/ClientApp/components/SeatsPerParty.tsx
@@ -0,0 +1,41 @@
+import { PartyResult } from "../interfaces/PartyResult";
+import * as React from "react";
+import { VictoryChart, VictoryBar } from "victory";
+
+export interface SeatsPerPartyProps {
+    showPartiesWithoutSeats: boolean;
+    partyResults: PartyResult[];
+}
+
+export class SeatsPerParty extends React.Component<SeatsPerPartyProps, {}> {
+    /**
+     * Takes raw data from property/properties, and converts them to more
+     * presentable things.
+     */
+    getData() {
+        if (this.props.showPartiesWithoutSeats) {
+            return this.props.partyResults;
+        } else {
+            return this.props.partyResults.filter(
+                party => party.totalSeats > 0
+            );
+        }
+    }
+
+    render() {
+        return (
+            <VictoryChart animate={false}
+                domainPadding={{ x: 20 }}>
+                <VictoryBar
+                    animate={{ duration: 666 }}
+                    data={this.getData().sort((a, b) => {
+                        return b.totalSeats - a.totalSeats;
+                    })}
+                    sortKey="t"
+                    x="partyCode"
+                    y="totalSeats"
+                />
+            </VictoryChart>
+        );
+    }
+}
