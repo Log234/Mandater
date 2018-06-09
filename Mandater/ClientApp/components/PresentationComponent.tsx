def346dc9655868161f1f05b7b0bea46a8eb2237
Unknown <ajsivesind@gmail.com>
2018-06-09 23:54:23 +0200

    refactor: Wrapped methods for handling presentation data

--- Mandater/ClientApp/components/PresentationComponent.tsx
+++ Mandater/ClientApp/components/PresentationComponent.tsx
@@ -5,6 +5,8 @@ import { ElectionOverview } from "./ElectionOverview";
 import { DistrictOverview } from "./DistrictOverview";
 import { SeatsPerParty } from "./SeatsPerParty";
 import { getDistrictTableData, getPartyTableData, getSeatsPerPartyData } from "../logic/PresentationUtilities";
+import { PartyResult } from "../interfaces/PartyResult";
+import { DistrictResult } from "../interfaces/DistrictResult";
 
 export interface PresentationProps {
     currentPresentation: PresentationType;
@@ -17,22 +19,28 @@ export class PresentationComponent extends React.Component<
     PresentationProps,
     {}
 > {
+    getPartyTableData(): PartyResult[] {
+        return getPartyTableData(this.props.results.partyResults, this.props.showPartiesWithoutSeats, this.props.decimals);
+    }
+
+    getDistrictTableData(): DistrictResult[] {
+        return getDistrictTableData(this.props.results.districtResults, this.props.decimals);
+    }
+
+    getSeatsPerPartyData(): PartyResult[] {
+        return getSeatsPerPartyData(this.props.results.partyResults, this.props.showPartiesWithoutSeats);
+    }
 
     render() {
-        const currentPresentation = this.props.currentPresentation;
-        const decimals = this.props.decimals;
-        const showPartiesWithoutSeats = this.props.showPartiesWithoutSeats;
-        const results = this.props.results;
-
-        switch (currentPresentation) {
+        switch (this.props.currentPresentation) {
             case PresentationType.ElectionTable:
-                return <ElectionOverview partyResults={getPartyTableData(results.partyResults, showPartiesWithoutSeats, decimals)} />;
+                return <ElectionOverview partyResults={this.getPartyTableData()} />;
             case PresentationType.DistrictTable:
-                return <DistrictOverview districtResults={getDistrictTableData(results.districtResults, decimals)} />;
+                return <DistrictOverview districtResults={this.getDistrictTableData()} />;
             case PresentationType.SeatsPerParty:
-                return <SeatsPerParty partyResults={getSeatsPerPartyData(results.partyResults, showPartiesWithoutSeats)} />;
+                return <SeatsPerParty partyResults={this.getSeatsPerPartyData()} />;
             default:
-                console.log(`Could not find presentation type ${currentPresentation}`);
+                console.log(`Could not find presentation type ${this.props.currentPresentation}`);
                 return <g />;
         }
     }
